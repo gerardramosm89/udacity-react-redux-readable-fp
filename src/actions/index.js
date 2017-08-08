3d6c19bfe77c4e5f603d91005e45ffd2eb2c1e9d
Gerard Ramos <gerard.ramosm89@gmail.com>
2017-08-07 22:17:34 -0700

    cleanup

--- src/actions/index.js
+++ src/actions/index.js
@@ -22,33 +22,24 @@ const options = {
 }
 export async function fetchCategories() {
   let categories = await axios.get(`${apiUrl}/categories`, options);
-  if (categories) {
     return {
       type: 'FETCH_CATEGORIES',
       payload: categories.data.categories
     }
-  }
 }
 
 export async function fetchAllPosts() {
   let allPosts = await axios.get(`${apiUrl}/posts`, options);
-  if (allPosts) {
     return {
       type: 'FETCH_ALL_POSTS',
       payload: allPosts.data
     }
-  }
 }
 
 export async function fetchPostsByCategory(category) {
   let categoryPosts = await axios.get(`${apiUrl}/${category}/posts`, options);
-  console.log(categoryPosts);
-  try {
     return {
       type: 'FETCH_POSTS_BY_CATEGORY',
       payload: categoryPosts.data
     }
-  } catch(e) {
-    console.log('ran into an error while fetching categories per post', e);
-  }
 }
