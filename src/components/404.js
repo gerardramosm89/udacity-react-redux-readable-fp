1b79290aa4ae0eae12629ce38891726439903495
Gerard Ramos <gerard.ramosm89@gmail.com>
2017-10-21 10:59:14 -0700

    adding styling to 404

--- src/components/404.js
+++ src/components/404.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default (props) => {
   return(
-    <div style={{display: 'flex', justifyContent: 'center'}}>
-      <h1 style={{}}>Post no longer exists, may have been deleted.</h1>
+    <div style={{display: 'flex', justifyContent: 'center', height: '90vh', alignItems: 'center'}}>
+      <h1 style={{fontSize: '6rem', textAlign: 'center' }}>Post no longer exists, may have been deleted.</h1>
     </div>
   );
 }
